18a4066eea2220427734cb5ff99323a5f401a3b1
renu <anilrenu08@gmail.com>
2025-03-11 12:21:46 -0400

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Ocr, TextDetections } from '@capacitor-community/image-to-text';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  imgSrc: SafeUrl | undefined;
+  lines: string[] = [];
+  message: string = '';
+  busy = false;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  async takePic() {
+    try {
+      this.busy = true;
+      const photo = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera        
+      });
+      const imageUrl = photo.webPath;
+      if (!imageUrl) return;
+      this.imgSrc = this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+      const path = photo.webPath;
+      if (!path) return;
+      this.lines = await this.processImage(path);
+    } catch (err) {
+      alert(err);
+    } finally {
+      this.busy = false;
+    }
+  }
+
+  async processImage(filename: string): Promise<string[]> {
+    const data: TextDetections = await Ocr.detectText({ filename });
+    // or with orientation -
+    // const textDetections = await td.detectText({ filename, orientation: ImageOrientation.Up })
+
+    console.log(data);
+    const result: string[] = [];
+    for (let detection of data.textDetections) {
+      result.push(detection.text); 
+      //`${detection.text} (${detection.bottomLeft},${detection.bottomRight},${detection.topLeft},${detection.topRight})`);
+    }
+    this.message = result.length === 0 ? 'No text was detected' : '';
+    return result;
+  }
+
+}
